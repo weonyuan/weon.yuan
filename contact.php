bbd3dc62744bbaaaf9ce54c10bfaa94b8746a960
Weon Yuan <weon.yuan@gmail.com>
2013-07-30 16:41:47 -1000

    Initial commit.

---
+++ contact.php
@@ -0,0 +1,122 @@
+<!DOCTYPE HTML>
+<html><!-- InstanceBegin template="/Templates/Main Layout.dwt" codeOutsideHTMLIsLocked="false" -->
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="shortcut icon" href="favicon.ico" />
+
+		<!-- InstanceBeginEditable name="doctitle" -->
+        <title>weon.yuan | Contact Me</title>
+        <!-- InstanceEndEditable -->
+        
+        <link rel="stylesheet" type="text/css" href="css/master.css" media="screen">
+        
+        <script type="text/javascript" src="scripts/jquery-1.8.2.js"></script>
+        <script type="text/javascript" src="scripts/superfish.js"></script>
+        <script type="text/javascript">
+            jQuery(function(){
+                jQuery('ul.sf-menu').superfish();
+            });
+    	</script>
+        
+        <!-- InstanceBeginEditable name="css/scripts" -->
+        
+        <!-- InstanceEndEditable -->
+    </head>
+
+    <body>
+    	<div id="mainWrapper">
+    	<div id="banner">
+    		<a href="index.htm"><img src="images/banner.png" width="260" height="80" alt="Banner" /></a>
+        </div>
+        
+        <div id="menuBar"> 
+            <ul class="sf-menu">
+                <li>
+                    <a href="index.htm">home</a>
+         		</li>
+                <li>
+                    <a href="projects.htm">projects</a>
+                </li>
+                <li>
+                    <a href="about.htm">about</a>
+                </li>
+                <li>
+                    <a href="resume.htm">resume</a>
+                </li>
+                <li>
+                    <a href="contact.php">contact me</a>
+                </li>
+            </ul>
+        </div>
+         
+        <div id="contentWrapper">
+			<!-- InstanceBeginEditable name="main-content" -->
+            <div class="header1">Contact Me</div>
+            
+            <table>
+            	<tr>
+                	<td>Name</td>
+                    <td><input type="text" name="Name" /></td>
+                </tr>
+                <tr>
+                	<td>Company</td>
+                    <td><input type="text" name="Company" /></td>
+                </tr>
+                <tr>
+                	<td>Email</td>
+                    <td><input type="email" name="Email" /></td>
+                </tr>
+                <tr>
+                	<td>Phone Number</td>
+                    <td><input type="tel" name="Phone Number" /></td>
+                </tr>
+                <tr>
+                	<td>Subject</td>
+                    <td>
+                    	<select name="Subject">
+                        	<option value="Comment">Comment</option>
+                            <option value="Employment">Employment</option>
+                            <option value="Problems/Concerns">Problems/Concerns</option>
+                            <option value="Technical Issues">Technical Issues</option>
+                            <option value="Misc">Misc</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                	<td>Message</td>
+                    <td><textarea name="Message" cols="30" rows="10"></textarea></td>
+                </tr>
+                <tr>
+                	<td colspan="2">CAPTCHA</td>
+               	</tr>
+                <tr>
+                	<td><input type="submit" value="Submit" /></td>
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
+            <!-- InstanceEndEditable -->
+       </div>
+       </div>
+
+       <footer>
+         <table>
+         	<tr>
+            	<td width="120" rowspan="2"><img src="images/logo.png" width="113" height="70" alt="Logo" /></td>
+                <td width="750">
+                	<a href="index.htm">Home</a> |
+                    <a href="projects.htm">Projects</a> |
+                    <a href="about.htm">About Me</a> |
+                    <a href="resume.htm">Resume</a> |
+                    <a href="contact.php">Contact Me</a>
+                </td>
+            </tr>
+            <tr>
+            	<td>
+                	<p style="border-top: 1px solid #FFFFFF;">&copy; 2013 Weon Yuan. All Rights Reserved.</p>
+                </td>
+            </tr>
+         </table>
+       </footer>
+       
+    </body>
+<!-- InstanceEnd --></html>
